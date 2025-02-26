6c33442d900c0a1d22e317a9aa84ac3c91db401e
Alee Hassan <alee@Alees-MacBook-Pro.local>
2025-02-26 06:58:51 +0500

    Add requirements.txt and update deployment configuration

--- medi.py
+++ medi.py
@@ -1,6 +1,10 @@
 import os
 import streamlit as st
 
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 
@@ -8,9 +12,12 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint
 
+
+
 ## Uncomment the following files if you're not using pipenv as your virtual environment manager
 #from dotenv import load_dotenv, find_dotenv
 #load_dotenv(find_dotenv())
+
 
 
 DB_FAISS_PATH="vectorstore/db_faiss"
